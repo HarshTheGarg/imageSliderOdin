91d6787cacab7cb0abf5c5c81858d23bf5077252
Harsh Garg <gargharsh2512@gmail.com>
2022-09-02 15:31:44 +0530

    Images will change automatically or when clicking the button
    Images change automatically after every few seconds. the automatic change stops when pointer is hovering and resumes when pointer leave the image. hover on either side of image to reveal the next/prev buttons, clicking on which will change the image.

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-const imagesContainer = document.querySelector('.images')
-const sideBarsContainer = document.querySelector('.sideBarsContainer')
+const images = document.querySelectorAll('.img')
+const container = document.querySelector('.images')
 const sideBars = document.querySelectorAll('.sb')
-
-window.addEventListener('resize', setSideBarHeight)
-
-function setSideBarHeight() {
-    sideBarsContainer.style.height = `${(imagesContainer.clientHeight - 4)}px`
-}
-
-setSideBarHeight()
 
 sideBars.forEach((element) => {
     element.addEventListener('mouseenter', () => {
@@ -17,4 +9,80 @@ sideBars.forEach((element) => {
     element.addEventListener('mouseleave', () => {
         element.style.opacity = 0
     })
+})
+
+document.querySelector('.sbLeft').addEventListener('click', () => {
+    prevImage()
+})
+
+document.querySelector('.sbRight').addEventListener('click', () => {
+    nextImage()
+})
+
+function nextImage() {
+    imgArr = Array.from(images)
+    
+    let flag = false
+
+    for(let i = 0; i < imgArr.length; i++){
+        if(imgArr[i].classList.contains('imgVis')){
+            imgArr[i].classList.remove('imgVis')
+            imgArr[i].parentElement
+            flag = true
+            if(i == imgArr.length - 1) {
+                imgArr[0].classList.add('imgVis')
+            }
+            continue
+        }
+        if(flag) {
+            imgArr[i].classList.add('imgVis')
+            break
+        }
+    }
+}
+
+function prevImage() {
+    imgArr = Array.from(images)
+    
+    let flag = false
+
+    for(let i = (imgArr.length - 1); i >= 0; i--){
+        if(imgArr[i].classList.contains('imgVis')){
+            imgArr[i].classList.remove('imgVis')
+            imgArr[i].parentElement
+            flag = true
+            if(i == 0) {
+                imgArr[(imgArr.length - 1)].classList.add('imgVis')
+            }
+            continue
+        }
+        if(flag) {
+            imgArr[i].classList.add('imgVis')
+            break
+        }
+    }
+}
+
+// var autoNextImage = window.setInterval(nextImage, 6000)
+
+var autoImage = (() => {
+    let autoNextImage
+    let start = () => {
+        autoNextImage = window.setInterval(nextImage, 4000)
+    }
+    let stop = () => {
+        clearInterval(autoNextImage)
+    }
+
+    return {start, stop}
+})()
+
+autoImage.start()
+
+container.addEventListener('mouseenter', () => {
+    autoImage.stop()
+})
+
+container.addEventListener('mouseleave', () => {
+    autoImage.start()
 })
